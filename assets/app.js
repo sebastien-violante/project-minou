f97469f1e27616a3433e1b294c1c9fbd68a4e49a
sebastien-violante <sebastien.violante@gmail.com>
2022-05-11 09:34:32 +0200

    11 mai 1

--- assets/app.js
+++ assets/app.js
@@ -3,9 +3,11 @@ const $ = require('jquery');
 global.$ = global.jQuery = $;
 
 import './bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+/* import 'bootstrap/dist/css/bootstrap.min.css'; */
 
 import './slick/slick.min.js';
 import './responsive-slick';
 
-
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
--- assets/responsive-slick.js
+++ assets/responsive-slick.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
     $('.responsive').slick({
         dots: true,
         infinite: false,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        speed: 100,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         responsive: [
           {
             breakpoint: 1024,
